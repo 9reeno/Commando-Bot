a2c6d4b278a4d04588c3cce7f14e8d1604bdd438
Connor Markwell <markwell.connor@gmail.com>
2017-04-11 12:42:38 -0400

    Added one more comment

--- commander.js
+++ commander.js
@@ -2,7 +2,7 @@ const Commando = require('discord.js-commando');
 const config = require('./config.js');
 
 const bot = new Commando.Client({
-  owner: '160927542092627968',
+  owner: '160927542092627968', // <- Discord account ID
   commandPrefix: '-'
 });
 
@@ -15,7 +15,7 @@ bot.registry
   .registerCommandsIn(__dirname+"/commands");
 
 bot
-  // Freeze bot until fully initialized
+  // Freeze bot  operation until fully initialized
   .on('ready', () => {
     console.log("Locked n' Loaded");
   })
